40b971d1010c1dad635dbdec84beca81aa219e0d
erik.romo@capgemini.com <erik.romo@capgemini.com>
2018-06-21 11:10:26 -0500

    Se agrega primer datos angular crud

---
+++ AngularCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,115 @@
+﻿using AngularCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace AngularCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>  
+        ///   
+        /// Get All Employee  
+        /// </summary>  
+        /// <returns></returns>  
+        public JsonResult Get_AllEmployee()
+        {
+            using (testEntities Obj = new testEntities())
+            {
+                List<employee> Emp = Obj.employees.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Get Employee With Id  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        public JsonResult Get_EmployeeById(string Id)
+        {
+            using (testEntities Obj = new testEntities())
+            {
+                int EmpId = int.Parse(Id);
+                return Json(Obj.employees.Find(EmpId), JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Insert New Employee  
+        /// </summary>  
+        /// <param name="Employe"></param>  
+        /// <returns></returns>  
+        public string Insert_Employee(employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (testEntities Obj = new testEntities())
+                {
+                    Obj.employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Delete Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Delete_Employee(employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (testEntities Obj = new testEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.employees.Attach(Emp);
+                        Obj.employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Update Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Update_Employee(employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (testEntities Obj = new testEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    employee EmpObj = Obj.employees.Where(x => x.id == Emp.id).FirstOrDefault();
+                    EmpObj.age = Emp.age;
+                    EmpObj.city = Emp.city;
+                    EmpObj.name = Emp.name;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+    }
+}
---
+++ AngularCRUD/Scripts/AngularCode.js
@@ -0,0 +1,96 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    debugger;
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Employe = {};
+            $scope.Employe.name = $scope.EmpName;
+            $scope.Employe.city = $scope.EmpCity;
+            $scope.Employe.age = $scope.EmpAge;
+            $http({
+                method: "post",
+                url: "http://localhost:54461/Employee/Insert_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+            })
+        } else {
+            $scope.Employe = {};
+            $scope.Employe.name = $scope.EmpName;
+            $scope.Employe.city = $scope.EmpCity;
+            $scope.Employe.age = $scope.EmpAge;
+            $scope.Employe.id = document.getElementById("EmpID_").value;
+            $http({
+                method: "post",
+                url: "http://localhost:54461/Employee/Update_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })
+        }
+    }
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:54461/Employee/Get_AllEmployee"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        $http({
+            method: "post",
+            url: "http://localhost:54461/Employee/Delete_Employee",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData();
+        })
+    };
+    $scope.UpdateEmp = function (Emp) {
+        document.getElementById("EmpID_").value = Emp.id;
+        $scope.EmpName = Emp.name;
+        $scope.EmpCity = Emp.city;
+        $scope.EmpAge = Emp.age;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Green";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+});
+app.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
